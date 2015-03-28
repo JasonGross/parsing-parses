67a60f5eb2d0aa959ee896ca76a676ccc1e9e29d
Jason Gross <jgross@mit.edu>
2015-03-28 14:41:07 -0400

    Remove unbuilt files

--- src/Parsers/Specification.v
+++
@@ -1,9 +0,0 @@
-(** * Definition of a [comp]-based specification of a CFG parser-recognizer *)
-Require Import ParsingParses.Computation.Core ParsingParses.Parsers.ContextFreeGrammar.
-
-Set Implicit Arguments.
-
-Definition parser_spec CharType (String : string_like CharType)
-           (G : grammar CharType)
-: String -> Comp bool
-  := fun str => { b : bool | b = true <-> inhabited (parse_of_grammar String str G) }%comp.
